6ca75ce28ad44c748fba4fd2544d685c793979d8
hadzhiDev <hadzhi.00703@gmail.com>
2025-02-25 12:55:10 +0600

    new domain

--- core/settings.py
+++ core/settings.py
@@ -152,6 +152,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1',
     'http://127.0.0.1:3000',
+    'https://south-cargo-osh.prolabagency.com',
+    'http://south-cargo-osh.prolabagency.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
